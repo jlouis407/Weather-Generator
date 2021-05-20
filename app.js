3d9fe843effcf02ab0988d73004e5ca07bc13a86
jlouis407 <josiah.israel.louis@gmail.com>
2021-05-20 18:47:54 -0400

    add css styling

--- app.js
+++ app.js
@@ -31,10 +31,9 @@ app.post("/", function(req, res){
             const weatherDescription = weatherData.weather[0].description;
             const icon = weatherData.weather[0].icon;
             const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
-            res.write("<p>The weather is currently " + weatherDescription + "</p>");
-            res.write("<h1>The temperature in " + query + " is " + temp + " degrees Celcius.</h1>");
-            res.write("<img src=" + imageURL + ">");
-            res.send();
+
+            res.render(
+                "index.ejs", {nameOfCity: query,temperature: temp, caption: weatherDescription, weatherImage: imageURL});
         })
     })
 
