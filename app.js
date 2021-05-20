e0f81c61cb95bf5918d8c70fb3cdc833ac145a3e
jlouis407 <josiah.israel.louis@gmail.com>
2021-05-20 12:50:37 -0400

    add ejs dependency, views folder, and public

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ const express = require("express");
 const https = require("https");
 const bodyParser = require("body-parser");
 
+
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
 
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/index.html");
